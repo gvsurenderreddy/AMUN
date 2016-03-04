ae30a8d36af13349e02da3a5717c06ad3aea04b4
tim4242 <tim4242@gmx.de>
2016-03-05 00:09:20 +0100

    Added possible logos

--- src/main/java/de/puzzleddev/amun/client/gui/IGui.java
+++ src/main/java/de/puzzleddev/amun/client/gui/IGui.java
@@ -1,12 +1,5 @@
 package de.puzzleddev.amun.client.gui;
 
-import de.puzzleddev.amun.client.gui.element.IGuiElement;
-
 public interface IGui
 {
-	public IGuiElement getRootElement();
-
-	public void update(double x, double y, int mouseBtn, int mouseEventType, int key, int keyboardEventType);
-
-	public void render();
 }
--- src/main/java/de/puzzleddev/amun/client/gui/transform/ITransformable.java
+++
@@ -1,6 +0,0 @@
-package de.puzzleddev.amun.client.gui.transform;
-
-public interface ITransformable
-{
-	public void applyTransformation(ITransformation transform);
-}
